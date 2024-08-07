bfb7272c3178bc72472f82f826db129e86da6169
Jude Osam-Sackey <43099552+judeo-s@users.noreply.github.com>
2024-08-07 13:32:18 +0000

    feat: done with task 4

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+"""
+Take the code from wait_n and alter it into a new function task_wait_n.
+"""
+
+from typing import List, Callable
+import asyncio
+
+
+task_wait_random: Callable = __import__("3-tasks").task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    This function returns a list of n random float numbers between 0 and
+    max_delay.
+    """
+    delays: List[float] = [task_wait_random(max_delay) for _ in range(n)]
+    return [await delay for delay in asyncio.as_completed(delays)]
