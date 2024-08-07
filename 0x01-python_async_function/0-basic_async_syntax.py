0a1144b721fa3cabfe54443a6c0f31bb4eae24fc
Jude Osam-Sackey <43099552+judeo-s@users.noreply.github.com>
2024-08-07 13:01:54 +0000

    fix: fixed a few suspected issues from the checker

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -8,6 +8,9 @@ import asyncio
 import random
 
 async def wait_random (max_delay: int = 10) -> float:
+    """
+    This function returns a random float number between 0 and max_delay.
+    """
     delay: float = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
     return delay
--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -5,12 +5,15 @@ A python script to learn about asyncronous programming
 """
 
 import asyncio
-import random
 from typing import List, Callable
 
 wait_random: Callable = __import__('0-basic_async_syntax').wait_random
 
-async wait_n(n: int, max_delay: int):
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    This function returns a list of n random float numbers between 0 and
+    max_delay.
+    """
     delays: List[float] = [
             asyncio.create_task(wait_random(max_delay)) for _ in range(n)
             ]
