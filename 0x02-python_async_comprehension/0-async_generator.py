0df7e2389d51b0f3fde0e3cc084318997fda4a12
Jude Osam-Sackey <43099552+judeo-s@users.noreply.github.com>
2024-08-08 01:13:21 +0000

    done with all tasks

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -5,13 +5,13 @@ A python script that generates a list asynchronously
 """
 import random
 import asyncio
-from typing import List
+from typing import Generator
 
 
-async def async_generator() -> List[float]:
+async def async_generator() -> Generator[float, None, None]:
     """
     An async function that yields a random number between 0 - 10
     """
     for _ in range(10):
-        yield random.uniform(0, 10)
         await asyncio.sleep(1)
+        yield random.uniform(0, 10)
