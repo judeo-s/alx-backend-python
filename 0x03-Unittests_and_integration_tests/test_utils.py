841cfd51ffa3509d8c160106050fde35f128ae75
Jude Osam-Sackey <43099552+judeo-s@users.noreply.github.com>
2024-08-27 20:59:53 +0000

    feat: implemented the solution for task 3

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -3,7 +3,7 @@
 """
 import unittest
 from parameterized import parameterized
-from utils import access_nested_map, get_json
+from utils import access_nested_map, get_json, memoize
 from unittest.mock import patch, Mock
 
 
@@ -44,6 +44,24 @@ class TestGetJson(unittest.TestCase):
         mock_get.return_value = Mock()
         mock_get.return_value.json.return_value = test_payload
         self.assertEqual(get_json(test_url), test_payload)
+
+
+class TestMemoize(unittest.TestCase):
+    """Test memoize."""
+
+    def test_memoize(self):
+        """Test memoize."""
+        class TestClass:
+
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+        test_class = TestClass()
+        self.assertEqual(test_class.a_property, 42)
+        self.assertEqual(test_class.a_property, 42)
 
 
 if __name__ == "__main__":
